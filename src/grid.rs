106bc90978d8f054c04b9372dd718485b04a3d68
Geordon Worley <vadixidav@gmail.com>
2016-08-29 20:42:48 -0400

    for long term simulation bump up movement cost

--- src/grid.rs
+++ src/grid.rs
@@ -10,7 +10,7 @@ const CONSUMPTION: f64 = 0.04;
 const SURVIVAL_THRESHOLD: f64 = 0.0;
 const DEATH_RELEASE_COEFFICIENT: f64 = 0.5;
 const INHALE_CAP: usize = 1000000;
-const MOVEMENT_COST: usize = 8;
+const MOVEMENT_COST: usize = 12;
 
 const FLUID_CYCLES: usize = 6;
 
